2ef20b7fb1182e0a3d45ff4d77e988f5031b36dd
Drew Powers <drew@pow.rs>
2020-02-09 23:30:46 -0700

    Add studios & animationSequences

--- src/components/PersonDetail/index.tsx
+++ src/components/PersonDetail/index.tsx
@@ -1,88 +1,114 @@
 import * as preact from '/web_modules/preact.js';
-import { Link } from '/web_modules/preact-router.js';
 
+import FilmGrid from '../FilmGrid/index.js';
+import { MetaTable, MetaKey, MetaValue } from '../MetaTable/index.js';
 import SkeletonText from '../SkeletonText/index.js';
 import SkeletonImg from '../SkeletonImg/index.js';
 import { OnePersonQuery } from '../../types/graphql';
 import { age, formatDate } from '../../utils/date.js';
+import StudioLink from '../StudioLink/index.js';
 
-interface FilmDetailProps {
+interface PersonDetailProps {
   person?: OnePersonQuery['person'];
 }
 
 const formatName = (person: OnePersonQuery['person']) =>
   person.alias ? person.alias : `${person.firstName} ${person.lastName}`;
 
-const formatFilm = (film: OnePersonQuery['person']['directed'][0]) => (
-  <Link activeClassName="is-active" className="personDetail-film" href={`/film/${film.id}`}>
-    <img className="personDetail-film-img" src={film.image.url} alt={film.image.name} />
-    <div className="personDetail-film-name">
-      {film.title}
-      {film.titleEN !== film.title && [<br />, film.titleEN]}
-      <br />
-      {film.releaseYear}
-    </div>
-  </Link>
-);
+/* eslint-disable no-param-reassign */
+function addToMeta(
+  films: OnePersonQuery['person']['directed'],
+  title: string,
+  meta: { [key: string]: string }
+) {
+  films.forEach(film => {
+    if (meta[film.id]) {
+      meta[film.id] += ` / ${title}`;
+    } else {
+      meta[film.id] = title;
+    }
+  });
+}
+/* eslint-enable no-param-reassign */
 
-const PersonDetail: preact.FunctionComponent<FilmDetailProps> = ({ person }) => {
+const PersonDetail: preact.FunctionComponent<PersonDetailProps> = ({ person }) => {
   if (person) {
     const name = formatName(person);
+    const filmMeta: { [key: string]: string } = {};
+    addToMeta(person.directed, 'Director', filmMeta);
+    addToMeta(person.wrote, 'Writer', filmMeta);
+    addToMeta(person.composed, 'Composer', filmMeta);
+    let films = [...person.directed, ...person.wrote, ...person.composed];
+    films = films.filter(({ id }, i) => films.findIndex(film => film.id === id) === i);
+    films.sort((a, b) => a.releaseYear - b.releaseYear);
 
     return (
       <div className="personDetail">
-        <img className="personDetail-img" src={person.image.url} alt={name} width="400" />
+        <div className="personDetail-img">
+          <img src={person.image.url} alt={person.image.title} width="400" />
+        </div>
         <div className="personDetail-details">
-          <h1 className="personDetail-title">{name}</h1>
+          <h1 className="personDetail-title">
+            {name} {!person.alias && person.kanji && `(${person.kanji})`}
+          </h1>
           <p>{person.description}</p>
-          <dl className="personDetail-stats">
-            {person.birthYear && [
-              <dt>Born</dt>,
-              <dd>
-                {formatDate({
-                  year: person.birthYear,
-                  month: person.birthMonth,
-                  day: person.birthDay,
-                })}
-              </dd>,
-              <dt>Age</dt>,
-              <dd>
-                {age(
-                  { year: person.birthYear, month: person.birthMonth, day: person.birthDay },
-                  {
-                    year: person.deathYear || new Date().getFullYear(),
-                    month: person.deathMonth || undefined,
-                    day: person.deathDay || undefined,
-                  }
-                )}
-              </dd>,
-            ]}
-            {person.deathYear && [
-              <dt>Dead</dt>,
-              <dd>
-                {formatDate({
-                  year: person.deathYear,
-                  month: person.deathMonth,
-                  day: person.deathDay,
-                })}
-              </dd>,
-            ]}
-          </dl>
-          {Array.isArray(person.directed) &&
-            person.directed.length > 0 && [
-              <h2 className="personDetail-subtitle">Directed</h2>,
-              person.directed.map(formatFilm),
-            ]}
-          {Array.isArray(person.animated) &&
-            person.animated.length > 0 && [
-              <h2 className="personDetail-subtitle">Animated</h2>,
-              person.animated.map(formatFilm),
-            ]}
-          {Array.isArray(person.composed) &&
-            person.composed.length > 0 && [
-              <h2 className="personDetail-subtitle">Composed</h2>,
-              person.composed.map(formatFilm),
-            ]}
+          <div className="personDetail-meta">
+            <MetaTable>
+              {person.alias && [
+                <MetaKey>Given Name</MetaKey>,
+                <MetaValue>
+                  {person.firstName} {person.lastName}
+                  {person.kanji && ` (${person.kanji})`}
+                </MetaValue>,
+              ]}
+              {person.birthYear && [
+                <MetaKey>Born</MetaKey>,
+                <MetaValue>
+                  {formatDate({
+                    year: person.birthYear,
+                    month: person.birthMonth,
+                    day: person.birthDay,
+                  })}
+                </MetaValue>,
+                <MetaKey>Age</MetaKey>,
+                <MetaValue>
+                  {age(
+                    { year: person.birthYear, month: person.birthMonth, day: person.birthDay },
+                    {
+                      year: person.deathYear || new Date().getFullYear(),
+                      month: person.deathMonth || undefined,
+                      day: person.deathDay || undefined,
+                    }
+                  )}
+                </MetaValue>,
+              ]}
+              {person.deathYear && [
+                <MetaKey>Died</MetaKey>,
+                <MetaValue>
+                  {formatDate({
+                    year: person.deathYear,
+                    month: person.deathMonth,
+                    day: person.deathDay,
+                  })}
+                </MetaValue>,
+              ]}
+              <MetaKey>Country</MetaKey>
+              <MetaValue>{person.country}</MetaValue>
+              {Array.isArray(person.founded) &&
+                person.founded.length > 0 && [
+                  <MetaKey>Founded</MetaKey>,
+                  <MetaValue>
+                    {person.founded.map(studio => (
+                      <StudioLink key={studio.id} studio={studio} />
+                    ))}
+                  </MetaValue>,
+                ]}
+            </MetaTable>
+          </div>
+        </div>
+        <div className="personDetail-films">
+          <h2 className="personDetail-subtitle">Films</h2>
+          <FilmGrid films={films} meta={filmMeta} />
         </div>
       </div>
     );
